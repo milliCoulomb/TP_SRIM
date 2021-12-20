fb4fd0efc65d7d19da3adba137c045a8c29df785
Universal-AC <mathis.caprais@gmail.com>
2021-12-20 10:14:37 +0100

    data for thorium

--- Alpha in UO2/alpha.py
+++ Alpha in UO2/alpha.py
@@ -26,14 +26,17 @@ linestyles = lss = [
 plt.rcParams.update({'font.size': 14})
 
 res_path = os.path.realpath(__file__)
-print(res_path+'\srim_output')
+# print(res_path+'\srim_output')
 data = Results('C:\\Users\\Mathis\\Desktop\\TP_SRIM\\Alpha in UO2\\srim_output')
 
 def plot_ioniz(res):
+    """
+    Plot the ionization due to electronic and ballistic interactions.
+    """
     ioniz = res.ioniz
     fig, ax = plt.subplots()
-    ax.set_xlabel(r'Depth (Angstroem)')
-    ax.set_ylabel(r'eV/Angstroem')
+    ax.set_xlabel(r'Depth ($\AA$)')
+    ax.set_ylabel(r'eV/$\AA$')
     ax.plot(ioniz.depth, ioniz.ions, label='Ionization from Ions')
     ax.plot(ioniz.depth, ioniz.recoils, label='Ionization from Recoils')
     plt.legend()
@@ -43,25 +46,14 @@ def plot_ioniz(res):
 
 
 def plot_etorecoils(res):
+    """
+    Plot the energy transfered from recoils.
+    """
     etorecoils = res.etorecoils
     fig, ax = plt.subplots()
-    ax.set_xlabel(r'Depth (Angstroem)')
-    ax.set_ylabel(r'eV/Angstroem')
+    ax.set_xlabel(r'Depth ($\AA$)')
+    ax.set_ylabel(r'eV/$\AA$')
     ax.plot(etorecoils.depth, etorecoils.ions, label='Energy from Ions')
-    #ax.plot(etorecoils.depth, etorecoils.recoils, label='Ionization from Recoils')
-    plt.legend()
-    plt.tight_layout()
-    fig.savefig('etorecoils.pdf')
-    plt.close(fig)
-
-
-def plot_etorecoils(res):
-    etorecoils = res.etorecoils
-    fig, ax = plt.subplots()
-    ax.set_xlabel(r'Depth (Angstroem)')
-    ax.set_ylabel(r'eV/Angstroem')
-    ax.plot(etorecoils.depth, etorecoils.ions, label='Energy from Ions')
-    #ax.plot(etorecoils.depth, etorecoils.recoils, label='Ionization from Recoils')
     plt.legend()
     plt.tight_layout()
     fig.savefig('etorecoils.pdf')
@@ -69,15 +61,35 @@ def plot_etorecoils(res):
 
 
 def plot_range(res):
+    """
+    Plot the distribution of incorporated projectiles.
+    """
     range = res.range
     fig, ax = plt.subplots()
-    ax.set_xlabel(r'Depth (Angstroem)')
+    ax.set_xlabel(r'Depth $\AA$)')
     ax.set_ylabel(r'Number')
     ax.plot(range.depth, range.ions, label='Ions range')
     plt.legend()
     plt.tight_layout()
     fig.savefig('range.pdf')
     plt.close(fig)
+
+
+def plot_collision(res):
+    """
+    Plot the collision events graph.
+    """
+    vacancies = res.vacancies
+    fig, ax = plt.subplots()
+    ax.set_xlabel(r'Depth ($\AA$)')
+    ax.set_ylabel(r'Number/$\AA$')
+    ax.plot(vacancies.depth, vacancies.knock_ons, label='Knock-ons')
+    ax.plot(vacancies.depth, vacancies.vacancies, label='Vacancies')
+    plt.legend()
+    plt.tight_layout()
+    fig.savefig('range.pdf')
+    plt.close(fig)
+
 
 
 plot_ioniz(data)
