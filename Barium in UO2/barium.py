09ab79e996b104206d258823d1a1d82c3b6340b0
Universal-AC <62665939+Universal-AC@users.noreply.github.com>
2021-12-26 11:26:40 +0100

    new plots for fission products

--- Barium in UO2/barium.py
+++ Barium in UO2/barium.py
@@ -43,4 +43,66 @@ def plot_collision(res, fluence, c, mod):
         fig.savefig('collisions.pdf')
         plt.close(fig)
 
+
+def plot_ioniz(res):
+    """
+    Plot the ionization due to electronic and ballistic interactions.
+    """
+    ioniz = res.ioniz
+    fig, ax = plt.subplots()
+    ax.set_xlabel(r'Depth ($\mu$m)')
+    ax.set_ylabel(r'eV/$\AA$')
+    ax.plot(ioniz.depth / 1e+4, ioniz.ions, label='Ionization from Ions')
+    ax.plot(ioniz.depth / 1e+4, ioniz.recoils, label='Ionization from Recoils')
+    plt.legend()
+    plt.tight_layout()
+    fig.savefig('ionization.pdf')
+    plt.close(fig)
+
+
+def plot_etorecoils(res):
+    """
+    Plot the energy transfered from recoils.
+    """
+    etorecoils = res.etorecoils
+    fig, ax = plt.subplots()
+    ax.set_xlabel(r'Depth ($\mu$m)')
+    ax.set_ylabel(r'eV/$\AA$')
+    ax.plot(etorecoils.depth / 1e+4, etorecoils.ions, label='Energy from Ions')
+    plt.legend()
+    plt.tight_layout()
+    fig.savefig('etorecoils.pdf')
+    plt.close(fig)
+
+
+def plot_range(res, fluence, mod):
+    """
+    Plot the distribution of incorporated projectiles.
+    """
+    if mod:
+        range = res.range
+        fig, ax = plt.subplots()
+        ax.set_xlabel(r'Depth ($\mu$m)')
+        ax.set_ylabel(r'Atoms/cm3 / Atoms / cm2')
+        ax.plot(range.depth / 1e+4, range.ions, label='Ions range')
+        plt.legend()
+        plt.tight_layout()
+        fig.savefig('range.pdf')
+        plt.close(fig)
+    else:
+        range = res.range
+        fig, ax = plt.subplots()
+        ax.set_xlabel(r'Depth ($\mu$m)')
+        ax.set_ylabel(r'Ba/cm3')
+        ax.plot(range.depth / 1e+4, range.ions * fluence, label='Concentration of Ba')
+        print("Max of concentration of Ba = "+"{:e}".format(np.max(range.ions * fluence))+" cm3")
+        plt.legend()
+        plt.tight_layout()
+        fig.savefig('range.pdf')
+        plt.close(fig)
+
+
 plot_collision(data, fluence, density, modify_coll)
+plot_etorecoils(data)
+plot_ioniz(data)
+plot_range(data, fluence, False)
